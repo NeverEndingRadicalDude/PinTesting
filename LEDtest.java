48edcaa41fd79b650b2b67316e014d3a79bb8c04
NeverEndingRadicalDude <110139042+NeverEndingRadicalDude@users.noreply.github.com>
2022-07-31 17:37:37 -0400

    Update LEDtest.java

--- LEDtest.java
+++ LEDtest.java
@@ -30,7 +30,7 @@ public class LEDtest {
       if (line.compareTo("") == 0) {
         finish = true;
       } else if (count == 0) {
-        updatePins(Integer.parseInt(line));
+        updatePins(Integer.parseInt(line), pin1, pin2, pin3, pin4, pin5, pin6, pin7 ,pin8 ,pin9, pin10);
         count = 1;
       } else if (count == 1) {
         Thread.sleep(Integer.parseInt(line));
