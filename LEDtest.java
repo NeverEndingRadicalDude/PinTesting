ae315bf3c696d9fd1fa09f2aee6d1b0bf70f16b1
NeverEndingRadicalDude <110139042+NeverEndingRadicalDude@users.noreply.github.com>
2022-07-31 17:25:10 -0400

    Update LEDtest.java

--- LEDtest.java
+++ LEDtest.java
@@ -124,7 +124,7 @@ public class Main {
    } else {
     try {
      updatePins(com, pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8, pin9, pin10);
-   } catch(InteruptedException) {
+   } catch(InteruptedException e) {
      readTxt(com, pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8, pin9, pin10);
     }
    }
